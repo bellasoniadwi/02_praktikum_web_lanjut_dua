6ad5a1f214089ab6414fb123cd5a78faf3d543fc
bellasoniadwi <bellasoniadwi@gmail.com>
2022-02-21 12:25:49 +0700

    ganti dengan controller single

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,10 @@ use Illuminate\Support\Facades\Route;
 /*Route::get('/', function () {
     return view('welcome');
 });*/
-USE App\Http\Controllers\PageController;
+USE App\Http\Controllers\HomeController;
+USE App\Http\Controllers\AboutController;
+USE App\Http\Controllers\ArticleController;
 
-Route::get('/',[PageController::class, 'index']);
-Route::get('/about',[PageController::class, 'about']);
-Route::get('/articles/{id}',[PageController::class, 'articles']);
+Route::get('/',[HomeController::class, 'index']);
+Route::get('/about',[AboutController::class, 'about']);
+Route::get('/articles/{id}',[ArticleController::class, 'articles']);
